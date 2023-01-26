4e3c0c9113546aec8c473d1033c87c1f683edc61
Bernhard Jordan <mail@bernhardjordan.space>
2023-01-26 10:41:37 +0100

    project setup, developed basic questionnaire

---
+++ Questionnaire/questionnaire/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Survey/>
+
+  </div>
+</template>
+
+<script>
+import Survey from './components/Survey.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Survey
+  },
+  mounted() {
+    // this.$store.dispatch('loadData');
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
---
+++ Questionnaire/questionnaire/src/components/Survey.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <Survey :survey="survey" />
+  </div>
+</template>
+
+<script>
+
+// import * as d3 from 'd3';
+import 'survey-core/defaultV2.min.css';
+import json from '../assets/surveydefinition.json'
+// StylesManager.applyTheme("defaultV2");
+import { Survey } from 'survey-vue-ui';
+import {Model } from 'survey-core';
+const surveyJson = json
+
+export default {
+  name: 'SurveyView',
+  components: {
+    Survey
+  },
+  props: {
+  },
+  data() {
+    const survey = new Model(surveyJson);
+    return {
+      survey
+      // svgWidth: 0,
+      // svgHeight: 500,
+      // svgPadding: {
+      //   top: 25, right: 20, bottom: 70, left: 40,
+      // },
+    }
+  },
+  mounted() {
+    // this.drawChart();
+  },
+  methods: {
+  },
+  computed: {
+    // disposablePersonaleIncome: {
+    //   get() {
+    //     return this.$store.getters.disposablePersonaleIncome;
+    //   }
+    // },
+    // dataMax() {
+    //   return d3.max(this.disposablePersonaleIncome, (d) => d.value);
+    // },
+    // dataMin() {
+    //   return d3.min(this.disposablePersonaleIncome, (d) => d.value);
+    // },
+    // xScale() {
+    //   return d3.scaleBand()
+    //     .rangeRound([0, this.svgWidth - this.svgPadding.left - this.svgPadding.right]).padding(0.1)
+    //     .domain(this.disposablePersonaleIncome.map((d) => d.state));
+    // },
+    // yScale() {
+    //   return d3.scaleLinear()
+    //     .rangeRound([this.svgHeight - this.svgPadding.top - this.svgPadding.bottom, 0])
+    //     .domain([this.dataMin > 0 ? 0 : this.dataMin, this.dataMax]);
+    // },
+  },
+  // watch: {
+  //   disposablePersonaleIncome: {
+  //     handler() {
+  //       this.drawChart();
+  //     },
+  //     deep: true,
+  //   },
+  // },
+}
+</script>
+
+<style>
+
+</style>
