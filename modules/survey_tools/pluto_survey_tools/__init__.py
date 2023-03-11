6e3780e213de07886ef37daf5dd27665fdb263b2
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-03-11 13:13:46 +0100

    refactor: move questionnaire.json to python module

--- modules/survey_tools/pluto_survey_tools/__init__.py
+++ modules/survey_tools/pluto_survey_tools/__init__.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from pluto_survey_tools.model import Questionnaire
 
-questionnaire_path = Path(__file__).parent.parent.parent.parent / "assets" / "questionnaire.json"
-questionnaire_dict = json.loads(questionnaire_path.read_text())
+questionnaire_path = Path(__file__).parent.parent / "assets" / "questionnaire.json"
+questionnaire_json_raw = questionnaire_path.read_text()
+questionnaire_dict = json.loads(questionnaire_json_raw)
 QUESTIONNAIRE = Questionnaire.from_dict(questionnaire_dict)
