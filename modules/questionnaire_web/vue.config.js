1147ef42c2d6a536c91782d808917a33dfcea387
Péter Gyarmati <dev.petergy@gmail.com>
2023-03-31 06:09:22 +0200

    feat(web): user interface enhancements (#1) (#2)
    * Pin node version
    
    * Add prettier for formatting
    
    * Format code
    
    * Fix `surveydefinition.json`
    
    Some question option IDs were duplicated and made no sense
    due to copy-pasting.
    
    * Include json files into formatting
    
    * Set app title through webpack
    
    * Add `survey-core` to explicit dep and bump its version
    
    * Left-align question header and content
    
    * Validate that either 'None of the above' or other options are selected
    
    * Use `otherText` and `showOtherItem` instead of `none*` when textbox is desired
    
    * Remove axis labels
    
    * Fix typo in store attribute name
    
    `feeback` to `feedback`
    
    * Add `.nvmrc`
    
    Allows to set node version by `nvm use`
    
    * Only render feedback headings if list is non empty
    
    * Rename project, format CSS with prettier
    
    * Add basic routing support to the app, create main styles
    
    * Create standalone header component
    
    * Fix survey typos, allow injecting HTML from json def
    
    * Fix home page styles
    
    * Fix result page styles
    
    * Add store stub for plain survey data transformation
    
    Needed by the server

--- modules/questionnaire_web/vue.config.js
+++ modules/questionnaire_web/vue.config.js
@@ -6,5 +6,13 @@ module.exports = {
         url: false,
       }
     }
+  },
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = "PLUTO - Public Value Assessment Tool";
+          return args;
+        })
   }
 }
