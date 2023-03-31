3a2a5487887027bafda321663ee94376cef6fa0b
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-03-31 05:53:34 +0200

    Create standalone header component

---
+++ modules/questionnaire_web/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-row style="background-color: #d8ead2">
+    <b-img
+      src="/PLUTO_logo.png"
+      alt="PLUTO logo"
+      fluid
+      class="logo"
+      width="200"
+    />
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
