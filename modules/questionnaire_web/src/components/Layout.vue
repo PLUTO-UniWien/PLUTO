a19c2a9f63782342dd517d867284d9e44533c35c
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-03-31 05:53:19 +0200

    Add basic routing support to the app, create main styles

---
+++ modules/questionnaire_web/src/components/Layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app" style="background-color: #f3f3f3; height: 100vh">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+}
+</script>
