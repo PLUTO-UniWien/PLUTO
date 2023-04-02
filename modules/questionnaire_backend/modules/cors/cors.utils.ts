f899620e4c92b5665ffedb4877d68fed1b9191ba
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-04-02 17:01:17 +0200

    feat: set allowed origins by node env

--- modules/questionnaire_backend/modules/cors/cors.utils.ts
+++ modules/questionnaire_backend/modules/cors/cors.utils.ts
@@ -1,6 +1,14 @@
 import NextCors from 'nextjs-cors'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+const DEV_ORIGINS = ['http://localhost:8080/']
+const PROD_ORIGINS = [
+  'https://pluto-survey.vercel.app/',
+  'https://pluto-survey-git-main-peter-gy.vercel.app/',
+  'http://pluto.vda.univie.ac.at/',
+  'https://pluto.vda.univie.ac.at/',
+]
+
 export default async function initCors<T>(
   req: NextApiRequest,
   res: NextApiResponse<T>
@@ -8,13 +16,7 @@ export default async function initCors<T>(
   await NextCors(req, res, {
     // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: [
-      'http://localhost:8080/',
-      'https://pluto-survey.vercel.app/',
-      'https://pluto-survey-git-main-peter-gy.vercel.app/',
-      'http://pluto.vda.univie.ac.at/',
-      'https://pluto.vda.univie.ac.at/',
-    ],
+    origin: process.env.NODE_ENV === 'development' ? DEV_ORIGINS : PROD_ORIGINS,
     optionsSuccessStatus: 200,
   })
 }
