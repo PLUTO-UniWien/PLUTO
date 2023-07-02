fd508b6a03700dd8e3dae28c0913b1ece59b06b3
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-07-02 14:57:38 +0200

    chore: scaffold Vue app with TS support

---
+++ apps/survey-public/configure-webpack.js
@@ -0,0 +1,33 @@
+const Dotenv = require('dotenv-webpack');
+
+/*
+ * Modify the webpack config by exporting an Object or Function.
+ *
+ * If the value is an Object, it will be merged into the final
+ * config using `webpack-merge`.
+ *
+ * If the value is a function, it will receive the resolved config
+ * as the argument. The function can either mutate the config and
+ * return nothing, OR return a cloned or merged version of the config.
+ *
+ * https://cli.vuejs.org/config/#configurewebpack
+ */
+
+/**
+ * @typedef {import('webpack').Configuration} WebpackOptions
+ */
+
+/**
+ * @param {WebpackOptions} config
+ * @returns {WebpackOptions}
+ */
+module.exports = (config) => {
+  const nodeEnv = process.env.NODE_ENV || 'development';
+  return {
+    plugins: [
+      new Dotenv({
+        path: `apps/survey-public/.env.${nodeEnv}`,
+      }),
+    ],
+  };
+};
