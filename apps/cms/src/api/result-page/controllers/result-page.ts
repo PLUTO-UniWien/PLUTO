8318bb77ea82b052a33950b60bfc8db075cd1e56
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-07-02 14:57:38 +0200

    feat: add survey and result page content types to cms

---
+++ apps/cms/src/api/result-page/controllers/result-page.ts
@@ -0,0 +1,7 @@
+/**
+ * result-page controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::result-page.result-page');
---
+++ apps/cms/src/api/result-page/routes/result-page.ts
@@ -0,0 +1,7 @@
+/**
+ * result-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::result-page.result-page');
