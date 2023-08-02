3da61443856b6be51026e68f9aa9cca9c694a70a
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-08-02 09:01:53 +0200

    fix: check CORS by domain name, ignore protocol

--- apps/survey-admin/src/middleware.ts
+++ apps/survey-admin/src/middleware.ts
@@ -2,7 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { env } from './env.mjs';
 import { corsRes } from '@pluto/utils';
 
-const allowedOrigins = env.NEXT_PUBLIC_ALLOWED_ORIGINS.split(',');
+const allowedOriginsNoProtocol = env.NEXT_PUBLIC_ALLOWED_ORIGINS.split(',');
+// Allow both http and https
+const allowedOrigins = new Set([
+  ...allowedOriginsNoProtocol.map((origin) => `http://${origin}`),
+  ...allowedOriginsNoProtocol.map((origin) => `https://${origin}`),
+]);
+
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const response = NextResponse.next({
@@ -12,7 +18,7 @@ export function middleware(request: NextRequest) {
   });
 
   const origin = requestHeaders.get('origin');
-  if (origin && allowedOrigins.includes(origin)) {
+  if (origin && allowedOrigins.has(origin)) {
     return corsRes(response, {
       origin,
       methods: request.method,
