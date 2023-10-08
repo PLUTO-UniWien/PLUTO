3589f250afda746897ae037ca01eadb914e421dd
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2023-10-08 16:36:39 +0200

    chore: add header to appendix stub

--- apps/survey-public/src/views/appendix/Appendix.vue
+++ apps/survey-public/src/views/appendix/Appendix.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="appendix">
+    <header-component />
     Appendix
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
+import HeaderComponent from '../../components/Header.vue';
 
 export default Vue.extend({
-  name: "AppendixView",
+  name: 'AppendixView',
+  components: { HeaderComponent },
 });
 </script>
