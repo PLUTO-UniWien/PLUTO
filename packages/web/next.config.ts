1fb80b851063154bb9458abb110d070f503b1d6c
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2025-03-24 22:09:52 +0100

    add env validation

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "node:url";
+import { createJiti } from "jiti";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// Importing env here to validate during build
+const jiti = createJiti(fileURLToPath(import.meta.url));
+try {
+  jiti.esmResolve("./src/env");
+  console.log("✅ Env validated during build");
+} catch (error) {
+  console.error("❌ Env validation failed during build", error);
+}
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
---
+++ packages/web/src/env.ts
@@ -0,0 +1,13 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    STRAPI_API_URL: z.string().url(),
+    STRAPI_API_TOKEN: z.string().min(1),
+  },
+  runtimeEnv: {
+    STRAPI_API_URL: process.env.STRAPI_API_URL,
+    STRAPI_API_TOKEN: process.env.STRAPI_API_TOKEN,
+  },
+});
