39c533c908a17030dd8e2bdeb11d33a2344423b1
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2025-03-29 20:24:21 +0100

    dynamic metadata for posts

--- packages/web/src/modules/posts/service.ts
+++ packages/web/src/modules/posts/service.ts
@@ -1,6 +1,7 @@
 import type { StrapiClient } from "@/modules/strapi/client";
 import type { APIResponseCollection } from "@/modules/strapi/types";
 import type { StrapiPost, StrapiPostPreview } from "./types";
+import type { StrapiSeo } from "@/modules/seo/types";
 
 export async function fetchPostPreviews(client: StrapiClient) {
   const posts = client.collection("posts");
@@ -22,4 +23,16 @@ export async function fetchPostBySlug(client: StrapiClient, slug: string) {
     throw new Error(`No post found with slug: ${slug}`);
   }
   return matchingPosts.data[0] as StrapiPost;
+}
+
+export async function fetchPostSeoBySlug(client: StrapiClient, slug: string) {
+  const posts = client.collection("posts");
+  const matchingPosts = (await posts.find({
+    filters: { slug: { eq: slug } },
+    populate: ["seo", "seo.metaImage", "seo.openGraph", "seo.openGraph.ogImage"],
+  })) as unknown as APIResponseCollection<"api::post.post">;
+  if (matchingPosts.data.length === 0) {
+    throw new Error(`No post found with slug: ${slug}`);
+  }
+  return matchingPosts.data[0].seo as StrapiSeo | undefined;
 }
